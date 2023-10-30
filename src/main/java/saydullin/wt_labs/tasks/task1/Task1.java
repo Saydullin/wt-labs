5c0f3b81a1b663ec266a72d5f0b1019848c0d5e1
Saydullin <saydullindev@gmail.com>
2023-10-30 23:40:58 +0300

    Added tasks

--- src/main/java/saydullin/wt_labs/tasks/task1/Task1.java
+++ src/main/java/saydullin/wt_labs/tasks/task1/Task1.java
@@ -9,6 +9,7 @@ public class Task1 {
         this.y = y;
     }
 
+
     public double execute() {
         double denominator = 1 + Math.pow(Math.sin(x + y), 2);
         double numerator = 2 + Math.abs(x - (2 * x / (1 + x^2 * y^2)));
